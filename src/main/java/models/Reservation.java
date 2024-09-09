167d937fa8c5839ad8b67bfb313a184a2a60986c
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-09 17:31:55 +0530

    Reservation.java file create

---
+++ src/main/java/models/Reservation.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.UUID;
+
+public class Reservation {
+    private String id;
+    private Flight flight;
+
+    public Reservation(Flight flight) {
+        this.id = UUID.randomUUID().toString();
+        this.flight = flight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+}
