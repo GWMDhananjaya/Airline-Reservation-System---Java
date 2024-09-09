61875c13b41e9927df8b47515fc56102dbddf1c7
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-09 17:29:31 +0530

    client.java file create

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client extends Person {
+    private List<Reservation> reservations;
+
+    public Client(String id, String name) {
+        super(id, name, "Client");
+        this.reservations = new ArrayList<>();
+    }
+
+    public void reserverVol(Flight flight) {
+        Reservation reservation = new Reservation(flight);
+        reservations.add(reservation);
+    }
+
+    public void annulerReservation(String reservationId) {
+        reservations.removeIf(reservation -> reservation.getId().equals(reservationId));
+    }
+
+    public List<Reservation> obtenirReservations() {
+        return reservations;
+    }
+}
