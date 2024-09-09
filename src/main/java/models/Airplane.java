bd215255d43305e3ff971dae151b4853148712cc
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-09 17:28:57 +0530

    Airplane.java file create

---
+++ src/main/java/models/Airplane.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Airplane {
+    private String id;
+    private boolean available;
+
+    public Airplane(String id) {
+        this.id = id;
+        this.available = true;
+    }
+
+    public void affecterVol() {
+        if (available) {
+            available = false;
+        }
+    }
+
+    public boolean verifierDisponibilite() {
+        return available;
+    }
+
+    // Add the getId() method
+    public String getId() {
+        return id;
+    }
+}
