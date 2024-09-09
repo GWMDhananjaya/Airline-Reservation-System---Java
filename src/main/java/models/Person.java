3aa540c1fc5653e6efec101c9fb0e8c2eda0eff1
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-09-09 17:31:14 +0530

    person.java file create

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Person {
+    private String id;
+    private String name;
+    private String role;
+
+    public Person(String id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void obtenirInfos() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Role: " + role);
+    }
+
+    public String obtenirRole() {
+        return role;
+    }
+}
